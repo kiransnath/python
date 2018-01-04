060f759c07e6e19372e90192101a33fc1eb94f08
Kiran Surendranath <32653151+kiransnath@users.noreply.github.com>
2018-01-04 11:51:30 -0800

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+print "\nUsage of dictionaries. Dictionaries are key-value pairs and use {key:value,key:value} but sets use {}";
+dict1 = {'s1':'kiran','s2':'job','s3':'kathie'};
+print dict1['s1'];
+dict2 = {'mark1':99,'mark2':90,'mark3':100};
+print 'Dictionalry 2 is {}'.format(dict2);
+print 'Mark2 is format(dict2['mark2']) '.format(dict2['mark2']);
+print '\n';
---
+++ tuples.py
@@ -0,0 +1,10 @@
+#This details the basic operations with tuples.
+print "\nTuples is similar to lists but read-only/unmutable. List use [] BUT Tuples use ()";
+print '\n';
+tup1 = (1,2,3);
+tup2 = (4,5,6);
+print "tup1 is {}".format(tup1);
+print "tup2 is {}".format(tup2);
+print "Adding of tuples tup1+tup2= {}".format(tup1+tup2);
+print "Multiplying tuple tup1*2= {}".format(tup1*2);
+print '\n';

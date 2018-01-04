db324394d3d77dce4930cbeb0f98499d44ee860d
Kiran Surendranath <32653151+kiransnath@users.noreply.github.com>
2018-01-04 12:01:08 -0800

    Add files via upload

--- dictionaries.py
+++ dictionaries.py
@@ -3,5 +3,5 @@ dict1 = {'s1':'kiran','s2':'job','s3':'kathie'};
 print dict1['s1'];
 dict2 = {'mark1':99,'mark2':90,'mark3':100};
 print 'Dictionalry 2 is {}'.format(dict2);
-print 'Mark2 is format(dict2['mark2']) '.format(dict2['mark2']);
+print "Mark2 is format(dict2['mark2']): {} ".format(dict2['mark2']);
 print '\n';
